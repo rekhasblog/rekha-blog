1c60fef24e9ceb9915c3eba84efec15802b24a14
Rekha <rekha.garcha@gmail.com>
2026-06-21 11:18:46 -0700

    Remove base path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://rekhasreflections.com',
-  base: '/rekha-blog',
 });
